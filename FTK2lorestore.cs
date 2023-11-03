967337219905120892dd92261688ebb72a7bfd72
ADDB <addb1234567890@gmail.com>
2023-11-03 13:35:22 +0100

    Hotkeys for Remove Lore, Make hidden items purchaseable and Sell everything.

--- FTK2lorestore.cs
+++ FTK2lorestore.cs
@@ -10,12 +10,15 @@ namespace ftk2lorestore {
     public class FTK2lorestore : BaseUnityPlugin {
         public const string PLUGIN_GUID = "ftk2lorestore";
         public const string PLUGIN_NAME = "FTK 2 Lore Store Cheats";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.1";
         public static readonly Harmony HarmonyInstance = new Harmony(PLUGIN_GUID);
         internal static ManualLogSource log;
         internal static BepInEx.Configuration.KeyboardShortcut addLore = new(UnityEngine.KeyCode.F2, UnityEngine.KeyCode.LeftShift);
         internal static BepInEx.Configuration.KeyboardShortcut unlockAll = new(UnityEngine.KeyCode.F3, UnityEngine.KeyCode.LeftShift);
-        internal static BepInEx.Configuration.KeyboardShortcut buyAll = new(UnityEngine.KeyCode.F4, UnityEngine.KeyCode.LeftShift);
+        internal static BepInEx.Configuration.KeyboardShortcut unlockHidden = new(UnityEngine.KeyCode.F4, UnityEngine.KeyCode.LeftShift);
+        internal static BepInEx.Configuration.KeyboardShortcut buyAll = new(UnityEngine.KeyCode.F5, UnityEngine.KeyCode.LeftShift);
+        internal static BepInEx.Configuration.KeyboardShortcut removeLore = new(UnityEngine.KeyCode.F6, UnityEngine.KeyCode.LeftShift);
+        internal static BepInEx.Configuration.KeyboardShortcut sellAll = new(UnityEngine.KeyCode.F7, UnityEngine.KeyCode.LeftShift);
         internal static UserData user => RouterMono.GetEnv().User;
         private void Awake() {
             // Plugin startup logic
@@ -29,23 +32,42 @@ namespace ftk2lorestore {
                 StatsHelper.AddStat("TOTAL_LORE", 50, user.Stats, false, true);
                 SaveGameHelper.SaveUser(user);
             }
+            if (removeLore.IsDown()) {
+                var current = StatsHelper.GetStat("TOTAL_LORE", user.Stats);
+                var dec = (current >= 50) ? 50 : current;
+                StatsHelper.AddStat("TOTAL_LORE", -dec, user.Stats, false, true);
+                SaveGameHelper.SaveUser(user);
+            }
             if (unlockAll.IsDown()) {
-                unlock();
+                Unlock();
                 SaveGameHelper.SaveUser(user);
             }
             if (buyAll.IsDown()) {
-                buy();
+                Buy();
                 SaveGameHelper.SaveUser(user);
             }
+            if (sellAll.IsDown()) {
+                Sell();
+                SaveGameHelper.SaveUser(user);
+            }
+            if (unlockHidden.IsDown()) {
+                Unlock(true);
+            }
         }
-        private static void unlock() {
+        private static void Unlock(bool onlyHidden = false) {
             foreach (var item in Env.Configs.LoreStore.Keys.ToList()) {
-                if (StatsHelper.GetStat(item, user.Stats) < 0 && Env.Configs.LoreStore[item].DefaultState >= -2) {
-                    StatsHelper.SetStat(item, 0, user.Stats, false, true);
+                if (onlyHidden) {
+                    if (StatsHelper.GetStat(item, user.Stats) == -2) {
+                        StatsHelper.SetStat(item, 0, user.Stats, false, true);
+                    }
+                } else {
+                    if (StatsHelper.GetStat(item, user.Stats) < 0 && Env.Configs.LoreStore[item].DefaultState >= -2) {
+                        StatsHelper.SetStat(item, 0, user.Stats, false, true);
+                    }
                 }
             }
         }
-        private static void buy() {
+        private static void Buy() {
             foreach (var item in Env.Configs.LoreStore.Keys.ToList()) {
                 if (!LoreStoreHelper.IsItemPurchased(item, user.Stats) && Env.Configs.LoreStore[item].DefaultState >= -2) {
                     StatsHelper.SetStat(item, 0, user.Stats, false, true);
@@ -53,6 +75,16 @@ namespace ftk2lorestore {
                 while (StatsHelper.GetStat(item, user.Stats) < Env.Configs.LoreStore[item].MaxState) {
                     StatsHelper.AddStat("TOTAL_LORE", LoreStoreHelper.GetItemCost(item, user.Stats), user.Stats, false, true);
                     LoreStoreHelper.PurchaseItem(item, user.Stats);
+                }
+            }
+        }
+        private static void Sell() {
+            foreach (var item in Env.Configs.LoreStore.Keys.ToList()) {
+                while (StatsHelper.GetStat(item, user.Stats) > 0) {
+                    StatsHelper.AddStat(item, -1, user.Stats, pPerRun: false, pCheckLoreUnlocks: true);
+                    StatsHelper.AddStat(eUserStats.LORE_STORE_PURCHASES, -1, user.Stats, pPerRun: false, pCheckLoreUnlocks: true);
+                    StatsHelper.AddStat(eUserStats.LORE_POINTS_SPENT, -Env.Configs.LoreStore[item].Cost, user.Stats, pPerRun: false, pCheckLoreUnlocks: true);
+                    StatsHelper.AddStat("TOTAL_LORE", Env.Configs.LoreStore[item].Cost, user.Stats, pPerRun: false, pCheckLoreUnlocks: true);
                 }
             }
         }
--- README.md
+++ README.md
@@ -4,9 +4,12 @@ The mod adds the following hotkeys (After using a Hotkey, if you already entered
 
 - LeftShift + F2: Add 50 Lore
 - LeftShift + F3: Make all items purchaseable
-- LeftShift + F4: Instantly Unlock Everything
+- LeftShift + F4: Make only hidden items purchaseable (stuff like preorder bonus and alpha unlockable I think)
+- LeftShift + F5: Instantly Unlock Everything
+- LeftShift + F6: Remove 50 Lore
+- LeftShift + F7: Sell everything. Refunds every purchase (but still keeps everything unlocked).
 
-### Installation
+## Installation
 
 Download the BepInEx Package from the Releases and unzip into your game directory to set it up.
 
@@ -32,3 +35,10 @@ Steamp/steamapps/common/For The King II/
 ├── MonoBleedingEdge/
 
 └── For The King II_Data/
+
+## Changelog
+## 1.0.1 (developed with For The King II 1.0.11
+- Hotkeys for Remove Lore, Make hidden items purchaseable and Sell everything.
+
+## 1.0.0 (developed with For The King II 1.0.9
+- Hotkeys for Add Lore, Make all items purchaseable and instantly unlock everything.
